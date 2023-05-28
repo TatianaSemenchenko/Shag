5afbb8930445ba20517f572e5235c52e865d2d3b
Tatiana Semenchenko <tatiana-semenchenko-m@yandex.by>
2023-05-28 19:25:49 +0300

    Draft

--- shag_class_inheritance.py
+++ shag_class_inheritance.py
@@ -12,33 +12,33 @@ class Human:
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
-        date_of_birth = datetime.datetime.strptime(date_of_birth, "%d/%m/%Y").date()
 
 
-    def set_age(self, age):
-        currentDate = datetime.datetime.today().date()
-        #some calculations here
-        age = currentDate.year - date_of_birth.year
-        monthVeri = currentDate.month - date_of_birth.month
-        dateVeri = currentDate.day - date_of_birth.day
+    def set_age(self):
+        currentdate = datetime.datetime.today().date()
+        date_of_birth = datetime.date.strptime(date_of_birth, "%d/%m/%Y").date()
+        age = currentdate.year - date_of_birth.year
+        monthveri = currentdate.month - date_of_birth.month
+        dateveri = currentdate.day - date_of_birth.day
 
         #Type conversion here
         age = int(age)
-        monthVeri = int(monthVeri)
-        dateVeri = int(dateVeri)
+        monthveri = int(monthveri)
+        dateveri = int(dateveri)
 
         # some decisions
-        if monthVeri < 0 :
+        if monthveri < 0 :
             age = age-1
-        elif dateVeri < 0 and monthVeri == 0:
+        elif dateveri < 0 and monthveri == 0:
             age = age-1
 
+        return age
 class Pupil(Human):
     """ Pupil's introduction."""
 
     def __init__(self, first_name, last_name, date_of_birth, school_class):
         Human.__init__(self, first_name, last_name, date_of_birth)
-        Human.set_age(self, age)
+        age = Human.set_age(self)
         self.school_class = school_class
 
 #        age = Pupil.set_age()
@@ -52,7 +52,7 @@ class Parent(Human):
         Human.__init__(self, first_name, last_name, date_of_birth)
         self.work = work
         self.working_address = working_address
-        age = Human.set_age(self, age)
+        age = Human.set_age(self)
         print( '"Parent\'s data: " {0} {1} {2} {3} {4} )'.format(first_name, last_name, age, work, working_address),
             end=" ")
 
